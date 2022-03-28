ba4fb19f47bc208c7194256adb373450feab5298
goruchan <ppcfr481@yahoo.co.jp>
2022-03-28 22:24:24 +0900

    [clean] mainメソッド追加
    上から下にコードを読んでいけるようにトップにmainメソッドを追加
    メソッド化する事でrubocopに指摘してもらえるようになる

--- 05.ls/lib/ls1.rb
+++ 05.ls/lib/ls1.rb
@@ -2,6 +2,44 @@
 # frozen_string_literal: true
 MAX_COLUMN = 3
 
+def main
+  file_paths = []
+  directory_paths = []
+  error_paths = []
+  
+   ARGV.each do |path|
+    if FileTest.directory?(path)
+      directory_paths.push(path)
+    elsif FileTest.file?(path)
+      file_paths.push(path)
+    else
+      error_paths.push(path)
+    end
+  end
+  
+  directory_paths.push('.') if(file_paths.size == 0 && directory_paths.size == 0 && error_paths.size == 0)
+  
+  if(file_paths.length > 0 || directory_paths.length > 0)
+    all_file_list = file_paths + retrieve_file_list(directory_paths)
+    padding_num = ( all_file_list.max {|a, b| a.length <=> b.length }.length ) +1
+  end
+  
+  if error_paths.size > 0
+    error_paths.sort_by{|x| x.to_i }.each{|error_path| puts "ls: " + error_path + ": No such file or directory" }
+  end
+  
+  if file_paths.size > 0
+    file_list = convert_array_for_view(file_paths.sort_by{|x| x.to_i })
+    view_file_list(file_list,padding_num)
+  end
+  
+  if directory_paths.size > 0
+    puts if error_paths.size > 0 || file_paths.size > 0
+    directory_file_list = retrieve_hash_list(directory_paths.sort_by{|x| x.to_i }).each{ |list| list[:file_list] = convert_array_for_view list[:file_list] }
+    view_hash_list(directory_file_list,padding_num)
+  end
+end
+
 def retrieve_hash_list(search_paths)
   target_paths_file_list = []
   search_paths.each do |path|
@@ -54,38 +92,4 @@ def view_file_list(file_list, padding_num)
   end
 end
 
-file_paths = []
-directory_paths = []
-error_paths = []
-
- ARGV.each do |path|
-  if FileTest.directory?(path)
-    directory_paths.push(path)
-  elsif FileTest.file?(path)
-    file_paths.push(path)
-  else
-    error_paths.push(path)
-  end
-end
-
-directory_paths.push('.') if(file_paths.size == 0 && directory_paths.size == 0 && error_paths.size == 0)
-
-if(file_paths.length > 0 || directory_paths.length > 0)
-  all_file_list = file_paths + retrieve_file_list(directory_paths)
-  padding_num = ( all_file_list.max {|a, b| a.length <=> b.length }.length ) +1
-end
-
-if error_paths.size > 0
-  error_paths.sort_by{|x| x.to_i }.each{|error_path| puts "ls: " + error_path + ": No such file or directory" }
-end
-
-if file_paths.size > 0
-  file_list = convert_array_for_view(file_paths.sort_by{|x| x.to_i })
-  view_file_list(file_list,padding_num)
-end
-
-if directory_paths.size > 0
-  puts if error_paths.size > 0 || file_paths.size > 0
-  directory_file_list = retrieve_hash_list(directory_paths.sort_by{|x| x.to_i }).each{ |list| list[:file_list] = convert_array_for_view list[:file_list] }
-  view_hash_list(directory_file_list,padding_num)
-end
+main
