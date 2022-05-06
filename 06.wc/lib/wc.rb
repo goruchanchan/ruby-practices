d2184f0411c5628979a41a601feed22658c0e3c5
goruchan <ppcfr481@yahoo.co.jp>
2022-05-06 17:23:15 +0900

    update mainメソッドを追加

--- 06.wc/lib/wc.rb
+++ 06.wc/lib/wc.rb
@@ -6,24 +6,28 @@ require 'optparse'
 
 require './lib/wc_command'
 
-params = { l_option: false }
+def main
+  params = { l_option: false }
 
-opt = OptionParser.new
-opt.on('-l') { |v| params[:l_option] = v }
-opt.parse!(ARGV)
+  opt = OptionParser.new
+  opt.on('-l') { |v| params[:l_option] = v }
+  opt.parse!(ARGV)
 
-if ARGV.empty?
-  puts run_wc(sentence: $stdin.read, **params)
-else
-  wc_contents = []
-  ARGV.each do |file_path|
-    pathname = Pathname(file_path)
-    if FileTest.exist?(file_path)
-      puts sentence_print = run_wc(file_path: pathname, sentence: File.open(file_path).read, **params)
-      wc_contents.push(sentence_print)
-    else
-      puts run_wc(file_path: pathname, sentence: nil)
+  if ARGV.empty?
+    puts run_wc(sentence: $stdin.read, **params)
+  else
+    wc_contents = []
+    ARGV.each do |file_path|
+      pathname = Pathname(file_path)
+      if FileTest.exist?(file_path)
+        puts sentence_print = run_wc(file_path: pathname, sentence: File.open(file_path).read, **params)
+        wc_contents.push(sentence_print)
+      else
+        puts run_wc(file_path: pathname, sentence: nil)
+      end
     end
+    puts calculate_total_size(wc_contents, params) if ARGV.size > 1
   end
-  puts calculate_total_size(wc_contents, params) if ARGV.size > 1
 end
+
+main
