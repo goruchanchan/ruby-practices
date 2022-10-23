851ad40748ad32bc47369d26e758b721f510424b
goruchan <ppcfr481@yahoo.co.jp>
2022-10-23 15:28:50 +0900

    update Frameクラス導入

--- 07.bowling_object/bowling_object.rb
+++ 07.bowling_object/bowling_object.rb
@@ -11,31 +11,61 @@ class Shot
   end
 end
 
-score = ARGV[0]
-scores = score.split(',')
-
-shots = scores.map { |s| Shot.new(s) }
-
-total_score = 0
-frame_num = 1
-one_frame_scores = []
-
-shots.each_with_index do |s, i|
-total_score += s.score
-one_frame_scores << s.score
-
-  if frame_num < 10 && one_frame_scores.sum == 10
-    total_score += if one_frame_scores.length == 1
-                     shots[i + 1].score + shots[i + 2].score # strike
-                   else
-                     shots[i + 1].score # spare
-                   end
+class Frame
+  attr_reader :first_shot, :second_shot
+  def initialize(first_shot, second_shot = nil, third_shot = nil)
+    @first_shot = first_shot
+    @second_shot = second_shot
+    @third_shot = third_shot
   end
 
-  if one_frame_scores.length == 2 || one_frame_scores.sum == 10
-    frame_num += 1
-    one_frame_scores.clear
+  def score
+    frame_score = @first_shot.score
+    frame_score += @second_shot.score unless @second_shot.nil?
+    frame_score += @third_shot.score unless @third_shot.nil?
+    frame_score
   end
 end
 
-puts total_score
+def calculate_score(shots)
+  frame_shots = []
+  frame_scores = []
+
+  shots.each_with_index do |s, i|
+    frame_shots << s
+    if frame_scores.size < 9
+      if frame_shots.size % 2 == 0 || s.score == 10
+        frame_scores << (s.score == 10 ? Frame.new(frame_shots[0]) : Frame.new(frame_shots[0], frame_shots[1]))
+        frame_shots.clear
+      end
+    else
+      if frame_shots.size == 3 || (frame_shots.size == 2 && frame_shots[0].score + frame_shots[1].score < 10)
+        frame_scores << (frame_shots.size == 3 ? Frame.new(frame_shots[0], frame_shots[1], frame_shots[2]) : Frame.new(frame_shots[0], frame_shots[1]))
+      end
+    end
+  end
+
+  total_score = 0
+
+  frame_scores.each_with_index do |s, i|
+    total_score += s.score
+
+    if i < 9 && s.score == 10
+      total_score += if s.second_shot.nil?
+                        frame_scores[i + 1].second_shot.nil? ? frame_scores[i + 1].first_shot.score * 2 : frame_scores[i + 1].first_shot.score + frame_scores[i + 1].second_shot.score# strike
+                      else
+                        frame_scores[i + 1].first_shot.score # spare
+                      end
+    end
+  end
+  total_score
+end
+
+def main
+  scores = ARGV[0].split(',')
+  shots = scores.map { |s| Shot.new(s) }
+
+  puts calculate_score(shots)
+end
+
+main
