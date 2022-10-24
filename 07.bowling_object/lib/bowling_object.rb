18a243203adb28e027bc7a3900d20549d4068939
goruchan <ppcfr481@yahoo.co.jp>
2022-10-24 22:15:37 +0900

    clean マジックナンバーで削除できそうな部分を削除

--- 07.bowling_object/lib/bowling_object.rb
+++ 07.bowling_object/lib/bowling_object.rb
@@ -66,13 +66,13 @@ class Game
 
   def sum_additional_marks
     @frames.each_with_index do |frame, i|
-      next if i > 8
+      next if i > @frames.length - 2 # 10フレームでは追加点計算をしない
 
       case frame.score_type
       when :strike
         @total_marks += @frames[i + 1].score_2shots
         # 9フレームにストライクで、10フレームでストライクを取っても次のフレームには移動しない
-        @total_marks += @frames[i + 2].score_1shot if @frames[i + 1].score_type == :strike && i < 8
+        @total_marks += @frames[i + 2].score_1shot if @frames[i + 1].score_type == :strike && i < @frames.length - 2
       when :spare
         @total_marks += @frames[i + 1].score_1shot
       end
