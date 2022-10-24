334efad2a09b9b81e945d7d28dbc54e036d64854
goruchan <ppcfr481@yahoo.co.jp>
2022-10-24 17:06:55 +0900

    clean メソッド名再修正
    変数のように見えるメソッドはあっても良いらしいので、変数ぽい方がわかりやすいと感じたのたで動詞としていたのを削除

--- 07.bowling_object/lib/bowling_object.rb
+++ 07.bowling_object/lib/bowling_object.rb
@@ -5,33 +5,33 @@ class Shot
   def initialize(mark)
     @mark = mark
   end
-  def get_shot_score
+  def score
     @mark == 'X' ? 10 : @mark.to_i
   end
 end
 
 class Frame
   def initialize(frame_shots)
-    @first_shot = frame_shots[0]
+    @first_shot  = frame_shots[0]
     @second_shot = frame_shots[1] unless frame_shots[1].nil?
-    @third_shot = frame_shots[2] unless frame_shots[2].nil?
+    @third_shot  = frame_shots[2] unless frame_shots[2].nil?
   end
 
-  def get_score_1shot
-    @first_shot.get_shot_score
+  def score_1shot
+    @first_shot.score
   end
 
-  def get_score_2shots
-    @second_shot.nil? ? get_score_1shot : get_score_1shot + @second_shot.get_shot_score
+  def score_2shots
+    @second_shot.nil? ? score_1shot : score_1shot + @second_shot.score
   end
 
-  def get_frame_score
-    @third_shot.nil? ? get_score_2shots : get_score_2shots + @third_shot.get_shot_score
+  def score_frame
+    @third_shot.nil? ? score_2shots : score_2shots + @third_shot.score
   end
 
-  def get_frame_type
-    return :strike if get_score_1shot == 10
-    return :spare if get_score_2shots == 10
+  def score_type
+    return :strike if score_1shot  == 10
+    return :spare  if score_2shots == 10
   end
 end
 
@@ -46,39 +46,36 @@ class Game
     frame_shots = []
     shots.each_with_index do |shot, i|
       frame_shots << shot
-      if @frames.size < 9 # 〜9投目
-        if frame_shots.size % 2 == 0 || shot.get_shot_score == 10
-          @frames << Frame.new(frame_shots)
-          frame_shots.clear
-        end
-      elsif(shots.length - 1 == i) # 10投目
+      if ( @frames.size < 9 && (frame_shots.size % 2 == 0 || shot.score == 10) ) # 〜9フレーム目
         @frames << Frame.new(frame_shots)
+        frame_shots.clear
       end
+      @frames << Frame.new(frame_shots) if shots.length - 1 == i # 10フレーム目
     end
   end
 
   def sum_down_marks
     @frames.each do |frame|
-      @total_marks += frame.get_frame_score
+      @total_marks += frame.score_frame
     end
   end
 
   def sum_additional_marks
     @frames.each_with_index do |frame,i|
-       if i < 9 
-        case frame.get_frame_type
+      if i < 9 
+        case frame.score_type
         when :strike
-          @total_marks += @frames[i + 1].get_score_2shots
+          @total_marks += @frames[i + 1].score_2shots
           # 9フレームにストライクで、10フレームでストライクを取っても次のフレームには移動しない
-          @total_marks += @frames[i + 2].get_score_1shot if @frames[i + 1].get_frame_type == :strike && i < 8
+          @total_marks += @frames[i + 2].score_1shot if @frames[i + 1].score_type == :strike && i < 8
         when :spare
-          @total_marks += @frames[i + 1].get_score_1shot
+          @total_marks += @frames[i + 1].score_1shot
         end
       end
     end
   end
 
-  def get_game_score
+  def game_score
     sum_down_marks
     sum_additional_marks
     @total_marks
@@ -86,7 +83,7 @@ class Game
 end
 
 def calculate_score(input)
-  Game.new( input.map { |shot| Shot.new(shot) } ).get_game_score
+  Game.new( input.map { |shot| Shot.new(shot) } ).game_score
 end
 
 def main
