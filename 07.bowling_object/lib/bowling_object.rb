4284b7636232a96a83e5e430036b23f6bcf42984
goruchan <ppcfr481@yahoo.co.jp>
2022-10-28 23:32:47 +0900

    clean rubocop対応

--- 07.bowling_object/lib/bowling_object.rb
+++ 07.bowling_object/lib/bowling_object.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-require_relative "shot"
-require_relative "frame"
-require_relative "game"
+require_relative 'shot'
+require_relative 'frame'
+require_relative 'game'
 
 def calculate_score(input)
   Game.new(input.map { |shot| Shot.new(shot) }).game_score
