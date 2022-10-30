f4013f1b680470d0e44512fdf25e512e8656f095
goruchan <ppcfr481@yahoo.co.jp>
2022-10-30 23:17:07 +0900

    clean 不要なインスタンス変数を削除

--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -3,32 +3,31 @@
 
 class Game
   def initialize(frames)
-    @total_marks = 0
     @frames = frames
   end
 
   def sum_down_marks
-    @total_marks = @frames.sum { |frame| frame.score}
+    @frames.sum { |frame| frame.score}
   end
 
   def sum_additional_marks
+    additional_marks = 0
     @frames.each_with_index do |frame, i|
       next if i > @frames.length - 2 # 10フレームでは追加点計算をしない
 
       case frame.score_type
       when :strike
-        @total_marks += @frames[i + 1].score_2shots
+        additional_marks += @frames[i + 1].score_2shots
         # 9フレームにストライクで、10フレームでストライクを取っても次のフレームには移動しないので条件付け
-        @total_marks += @frames[i + 2].score_1shot if @frames[i + 1].score_type == :strike && i < @frames.length - 2
+        additional_marks += @frames[i + 2].score_1shot if @frames[i + 1].score_type == :strike && i < @frames.length - 2
       when :spare
-        @total_marks += @frames[i + 1].score_1shot
+        additional_marks += @frames[i + 1].score_1shot
       end
     end
+    additional_marks
   end
 
   def score
-    sum_down_marks
-    sum_additional_marks
-    @total_marks
+    sum_down_marks + sum_additional_marks
   end
 end
