792e19b14eda84d4bf82b1b8622e85b2e5a02564
goruchan <ppcfr481@yahoo.co.jp>
2022-11-06 14:40:00 +0900

    clean rubocop対応

--- 07.bowling_object/lib/game.rb
+++ 07.bowling_object/lib/game.rb
@@ -12,13 +12,14 @@ class Game
 
   def sum_additional_marks
     9.times.sum do |i|
-      if @frames[i].score_type == :strike
-        if @frames[i + 1].score_type == :strike && i < @frames.length - 2# 9フレームにストライクで、10フレームでストライクを取っても次のフレームには移動しないので条件付け
+      case @frames[i].score_type
+      when :strike
+        if @frames[i + 1].score_type == :strike && i < @frames.length - 2 # 9フレームにストライクで、10フレームでストライクを取っても次のフレームには移動しないので条件付け
           @frames[i + 1].score_2shots + @frames[i + 2].score_1shot
         else
           @frames[i + 1].score_2shots
         end
-      elsif @frames[i].score_type == :spare
+      when :spare
         @frames[i + 1].score_1shot
       else
         0
