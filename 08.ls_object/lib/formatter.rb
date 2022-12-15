8660c8b3162c0138fbbaabb164761df1d7100cf1
goruchan <ppcfr481@yahoo.co.jp>
2022-12-15 22:10:43 +0900

    clean キーワード引数に変更

--- 08.ls_object/lib/formatter.rb
+++ 08.ls_object/lib/formatter.rb
@@ -32,48 +32,48 @@ class Formatter
   def long_format
     @groups.map do |group|
       if group.title.nil?
-        long_message(convert_list_segment(group.files, '.'))
+        long_message(files: to_long_files(files: group.files, path: '.'))
       else
-        long_files = convert_list_segment(group.files, group.title)
+        long_files = to_long_files(files: group.files, path: group.title)
         title = @not_nil_group_num > 1 ? "#{group.title}:\n" : ''
-        "#{title}total #{total_block_size(group.files, group.title)}\n#{long_message(long_files)}"
+        "#{title}total #{total_block_size(files: group.files, path: group.title)}\n#{long_message(files: long_files)}"
       end
     end.join("\n\n")
   end
 
-  def convert_list_segment(names, path)
-    names.map { |name| construct_long_segment(name, path) }
+  def to_long_files(files:, path:)
+    files.map { |file| to_long_file(file: file, path: path) }
   end
 
-  def total_block_size(files, path)
+  def total_block_size(files:, path:)
     files.map { |file| File.lstat("#{path}/#{file}").blocks }.sum
   end
 
-  def construct_long_segment(name, path)
-    full_name = "#{path}/#{name}"
-    long_info = File.lstat(full_name) # statだとシンボリックリンクのパスが元ファイルになってしまうので、lstat
+  def to_long_file(file:, path:)
+    full_path = "#{path}/#{file}"
+    long_info = File.lstat(full_path) # statだとシンボリックリンクのパスが元ファイルになってしまうので、lstat
     month = long_info.mtime.to_a[4].to_s.rjust(2)
     day = long_info.mtime.to_a[3].to_s.rjust(2)
     clock = long_info.mtime.to_a[2].to_s.rjust(2, '0')
     minitus = long_info.mtime.to_a[1].to_s.rjust(2, '0')
-    name = "#{name} -> #{File.readlink(full_name)}" if long_info.symlink?
-    ["#{file_type(full_name)}#{file_permit(long_info.mode)}",
+    file = "#{file} -> #{File.readlink(full_path)}" if long_info.symlink?
+    ["#{file_type(full_path: full_path)}#{file_permit(mode: long_info.mode)}",
      long_info.nlink, Etc.getpwuid(long_info.uid).name, Etc.getgrgid(long_info.gid).name, long_info.size,
-     "#{month} #{day} #{clock}:#{minitus}", name]
+     "#{month} #{day} #{clock}:#{minitus}", file]
   end
 
-  def file_type(name)
-    { file: '-', directory: 'd', link: 'l' }[File.ftype(name).intern]
+  def file_type(full_path:)
+    { file: '-', directory: 'd', link: 'l' }[File.ftype(full_path).intern]
   end
 
-  def file_permit(mode)
+  def file_permit(mode:)
     owener = ((mode >> 6) % 8)
     group = ((mode >> 3) % 8)
     other = mode % 8
     "#{PERMISSIONS[owener]}#{PERMISSIONS[group]}#{PERMISSIONS[other]}"
   end
 
-  def long_message(files)
+  def long_message(files:)
     paddings = (0..6).map { |n| Matrix.columns(files).row(n).max.to_s.length }
 
     files.map do |file|
@@ -92,23 +92,23 @@ class Formatter
     @groups.map do |group|
       result = group.title.nil? || @not_nil_group_num < 2 ? '' : "#{group.title}:\n"
       max_char_length = group.title.nil? ? search_max_char_length + 1 : search_max_char_length + 3 # directoryの方が2つ半角が多い
-      result + normal_message(convert_array(group.files), max_char_length)
+      result + normal_message(files: to_matrix(files: group.files), max_char_length: max_char_length)
     end.join("\n\n")
   end
 
-  def convert_array(list)
-    if list.length % MAX_COLUMN != 0 # 行列変換させるために足りない要素にnilをつめていく
-      start_fill_nil = list.length + 1
-      end_fill_nil = (((list.length / MAX_COLUMN) + 1) * MAX_COLUMN - 1)
-      column = (list.length / MAX_COLUMN) + 1
-      list.fill(nil, start_fill_nil..end_fill_nil)
+  def to_matrix(files:)
+    if files.length % MAX_COLUMN != 0 # 行列変換させるために足りない要素にnilをつめていく
+      start_fill_nil = files.length + 1
+      end_fill_nil = (((files.length / MAX_COLUMN) + 1) * MAX_COLUMN - 1)
+      column = (files.length / MAX_COLUMN) + 1
+      files.fill(nil, start_fill_nil..end_fill_nil)
     else
-      column = (list.length / MAX_COLUMN) # 転置してMAX_COLUMN列にするので、sliceではMAX_COLUMN行にする
+      column = (files.length / MAX_COLUMN) # 転置してMAX_COLUMN列にするので、sliceではMAX_COLUMN行にする
     end
-    list.each_slice(column).map { |split_array| split_array }.transpose
+    files.each_slice(column).map { |split_array| split_array }.transpose
   end
 
-  def normal_message(files, max_char_length)
+  def normal_message(files:, max_char_length:)
     files.map do |file|
       file.map.with_index { |name, i| file[i + 1].nil? ? name.to_s : name.to_s.ljust(max_char_length) }.join
     end.join("\n")
--- 08.ls_object/lib/input.rb
+++ 08.ls_object/lib/input.rb
@@ -25,10 +25,10 @@ class Input
     @paths.each { |path| FileTest.directory?(path) ? directories_path.push(path) : files.push(path) }
 
     @groups.push(FileGroup.new(title: nil, files: files)) unless files.empty?
-    directories_path.each { |path| @groups.push(FileGroup.new(title: path, files: parse_option(path))) }
+    directories_path.each { |path| @groups.push(FileGroup.new(title: path, files: parse_option(path: path))) }
   end
 
-  def parse_option(path)
+  def parse_option(path:)
     names = @option_all ? Dir.glob('*', File::FNM_DOTMATCH, base: path).push('..') : Dir.glob('*', base: path)
     @option_reverse ? names.sort_by(&:to_s).reverse : names.sort_by(&:to_s)
   end
