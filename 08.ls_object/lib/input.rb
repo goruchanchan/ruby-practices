d03575090a83d33405da394d2399445bc14202da
goruchan <ppcfr481@yahoo.co.jp>
2022-12-16 23:05:17 +0900

    clean 分ける必要のないメソッドを削除

--- 08.ls_object/lib/input.rb
+++ 08.ls_object/lib/input.rb
@@ -21,19 +21,18 @@ class Input
 
   def separate_by_type
     files = []
-    directories = []
-    @paths.each { |path| FileTest.directory?(path) ? directories.push(path) : files.push(path) }
-    { files: files, directories: directories }
+    directory_paths = []
+    @paths.each { |path| FileTest.directory?(path) ? directory_paths.push(path) : files.push(path) }
+    { files: files, directory_paths: directory_paths }
   end
 
   def create_groups
     separated_group = separate_by_type
     @groups.push(FileGroup.new(nil, separated_group[:files])) unless separated_group[:files].empty?
-    separated_group[:directories].each { |directory| @groups.push(FileGroup.new(directory, parse_option(path: directory))) }
-  end
-
-  def parse_option(path:)
-    names = @option_all ? Dir.glob('*', File::FNM_DOTMATCH, base: path).push('..') : Dir.glob('*', base: path)
-    @option_reverse ? names.sort_by(&:to_s).reverse : names.sort_by(&:to_s)
+    separated_group[:directory_paths].each do |directory_path|
+      names = @option_all ? Dir.glob('*', File::FNM_DOTMATCH, base: directory_path).push('..') : Dir.glob('*', base: directory_path)
+      names = @option_reverse ? names.sort_by(&:to_s).reverse : names.sort_by(&:to_s)
+      @groups.push(FileGroup.new(directory_path, names))
+    end
   end
 end
